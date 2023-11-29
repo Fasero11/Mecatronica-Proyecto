947e1798df3dd5cec38c0c0813ccdc0859a4cf2d
Alvaro Alonso Alosete <a.alonsoa.2020@alumnos.urjc.es>
2023-11-29 14:00:03 +0100

    touch_songs added

--- music/get_song_notes.py
+++ music/get_song_notes.py
@@ -1,6 +1,6 @@
+import serial
 from music21 import converter, instrument, note, stream, chord
 import mido
-import serial
 import sys
 
 def check_arg():
@@ -48,15 +48,15 @@ def convert_to_clave_de_sol(notes):
 def convert_clave_sol_to_numbers(notes):
     # Asignar números a las notas en función de la tabla de equivalencia
     equivalence_table = {
-        'Do0': 1,  'Do#0': 2,  'Re0': 3,  'Re#0': 4,  'Mi0': 5,  'Fa0': 6,  'Fa#0': 7,  'Sol0': 8,  'Sol#0': 9,  'La0': 10, 'La#0': 11, 'Si0': 12,
-        'Do1': 13, 'Do#1': 14, 'Re1': 15, 'Re#1': 16, 'Mi1': 17, 'Fa1': 18, 'Fa#1': 19, 'Sol1': 20, 'Sol#1': 21, 'La1': 22, 'La#1': 23, 'Si1': 24,
-        'Do2': 25, 'Do#2': 26, 'Re2': 27, 'Re#2': 28, 'Mi2': 29, 'Fa2': 30, 'Fa#2': 31, 'Sol2': 32, 'Sol#2': 33, 'La2': 34, 'La#2': 35, 'Si2': 36,
-        'Do3': 37, 'Do#3': 38, 'Re3': 39, 'Re#3': 40, 'Mi3': 41, 'Fa3': 42, 'Fa#3': 43, 'Sol3': 44, 'Sol#3': 45, 'La3': 46, 'La#3': 47, 'Si3': 48,
-        'Do4': 49, 'Do#4': 50, 'Re4': 51, 'Re#4': 52, 'Mi4': 53, 'Fa4': 54, 'Fa#4': 55, 'Sol4': 56, 'Sol#4': 57, 'La4': 58, 'La#4': 59, 'Si4': 60,
-        'Do5': 61, 'Do#5': 62, 'Re5': 63, 'Re#5': 64, 'Mi5': 65, 'Fa5': 66, 'Fa#5': 67, 'Sol5': 68, 'Sol#5': 69, 'La5': 70, 'La#5': 71, 'Si5': 72,
-        'Do6': 73, 'Do#6': 74, 'Re6': 75, 'Re#6': 76, 'Mi6': 77, 'Fa6': 78, 'Fa#6': 79, 'Sol6': 80, 'Sol#6': 81, 'La6': 82, 'La#6': 83, 'Si6': 84,
-        'Do7': 85, 'Do#7': 86, 'Re7': 87, 'Re#7': 88, 'Mi7': 89, 'Fa7': 90, 'Fa#7': 91, 'Sol7': 92, 'Sol#7': 93, 'La7': 94, 'La#7': 95, 'Si7': 96,
-        'Do8': 97, 'Do#8': 98, 'Re8': 99, 'Re#8': 100,'Mi8': 101,'Fa8': 102,'Fa#8': 103,'Sol8': 104,'Sol#8': 105,'La8': 106,'La#8': 107,'Si8': 108
+        'Do0': 1,  'Do#0': 2,  'Re0': 19, 'Re#0': 20, 'Mi0': 37, 'Fa0': 46, 'Fa#0': 47, 'Sol0': 64, 'Sol#0': 65, 'La0': 82, 'La#0': 83, 'Si0': 100,
+        'Do1': 3,  'Do#1': 4,  'Re1': 21, 'Re#1': 22, 'Mi1': 38, 'Fa1': 48, 'Fa#1': 49, 'Sol1': 66, 'Sol#1': 67, 'La1': 84, 'La#1': 85, 'Si1': 101,
+        'Do2': 5,  'Do#2': 6,  'Re2': 23, 'Re#2': 24, 'Mi2': 39, 'Fa2': 50, 'Fa#2': 51, 'Sol2': 68, 'Sol#2': 69, 'La2': 86, 'La#2': 87, 'Si2': 102,
+        'Do3': 7,  'Do#3': 8,  'Re3': 25, 'Re#3': 26, 'Mi3': 40, 'Fa3': 52, 'Fa#3': 53, 'Sol3': 70, 'Sol#3': 71, 'La3': 88, 'La#3': 89, 'Si3': 103,
+        'Do4': 9,  'Do#4': 10, 'Re4': 27, 'Re#4': 28, 'Mi4': 41, 'Fa4': 54, 'Fa#4': 55, 'Sol4': 72, 'Sol#4': 73, 'La4': 90, 'La#4': 91, 'Si4': 104,
+        'Do5': 11, 'Do#5': 12, 'Re5': 29, 'Re#5': 30, 'Mi5': 42, 'Fa5': 56, 'Fa#5': 57, 'Sol5': 74, 'Sol#5': 75, 'La5': 92, 'La#5': 93, 'Si5': 105,
+        'Do6': 13, 'Do#6': 14, 'Re6': 31, 'Re#6': 32, 'Mi6': 43, 'Fa6': 58, 'Fa#6': 59, 'Sol6': 76, 'Sol#6': 77, 'La6': 94, 'La#6': 95, 'Si6': 106,
+        'Do7': 15, 'Do#7': 16, 'Re7': 33, 'Re#7': 34, 'Mi7': 44, 'Fa7': 60, 'Fa#7': 61, 'Sol7': 78, 'Sol#7': 79, 'La7': 96, 'La#7': 97, 'Si7': 107,
+        'Do8': 17, 'Do#8': 18, 'Re8': 35, 'Re#8': 36, 'Mi8': 45, 'Fa8': 62, 'Fa#8': 63, 'Sol8': 80, 'Sol#8': 81, 'La8': 98, 'La#8': 99, 'Si8': 108
     }
     
     converted_notes = []
@@ -71,28 +71,17 @@ def extract_tempo(file_path):
         for msg in track:
             if msg.type == 'set_tempo':
                 tempo = mido.tempo2bpm(msg.tempo)
-                return [tempo]
+                return tempo
     return None
-
-def send_notes(notes):
-
-    # Abre la conexión con el puerto serie
-    ser = serial.Serial('COM3', 9600)  # Ajusta 'COM3' al puerto correcto de tu Arduino
-
-    # Envía datos a Arduino
-    ser.write(notes.encode())
-
-    # Cierra la conexión
-    ser.close()
-
+    
 
 # Usage
 file_path = check_arg()
 notes = midi_to_notes(file_path)
 notes_clave_sol = convert_to_clave_de_sol(notes)
-#print(notes_clave_sol)
+print(notes_clave_sol)
 notas_numeros = convert_clave_sol_to_numbers(notes_clave_sol)
-#print(notas_numeros)
+print(notas_numeros)
 
 tempo = extract_tempo(file_path)
 if tempo:
@@ -100,7 +89,16 @@ if tempo:
 else:
     print("No se encontró información de tempo en el archivo MIDI.")
 
-notes_message = tempo + notas_numeros
-print(notes_message)
-#send_notes(notes_message)
+#notes_message = tempo + notas_numeros
+tempo_msg = "Tempo: " + str(tempo)
+print(tempo_msg)
 
+
+# Abre la conexión con el puerto serie
+#ser = serial.Serial('/dev/ttyUSB0', 9600)  # Ajusta '/dev/ttyUSB0' al puerto correcto de tu Arduino 
+#print(notes_clave_sol)
+## Envía datos a Arduino
+#ser.write(tempo_msg.encode())
+## Cierra la conexión
+#ser.close()
+#
