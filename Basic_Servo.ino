b35ae4ce48aaa078010c850227e5abb236a99d9d
Fasero11 <86266311+Fasero11@users.noreply.github.com>
2023-11-29 13:19:04 +0100

    Add files via upload

--- Basic_Servo.ino
+++ Basic_Servo.ino
@@ -1,7 +1,17 @@
-#include <Servo.h>
+#include <ESP32Servo.h>
+#include <BluetoothSerial.h>
+
+#define PIN_SERVO0 23
+#define PIN_SERVO1 22
+#define PIN_SERVO2 21
+#define PIN_SERVO3 19
+#define PIN_SERVO4 18
+#define PIN_SERVO5 17
+#define PIN_SERVO6 16
+#define PIN_SERVO7 2
+#define PIN_SERVO8 15
 
 Servo servo0;
-/*
 Servo servo1;
 Servo servo2;
 Servo servo3;
@@ -10,47 +20,97 @@ Servo servo5;
 Servo servo6;
 Servo servo7;
 Servo servo8;
-*/
-int melody[] = {0,1,0,0,0,1,1,0,1};
+
+BluetoothSerial SerialBT;
+
+int song1[] = {0,1,0,0,0,1,1,0,1};
+int song2[] = {0,1,0,0,0,1,1,0,1};
+int song3[] = {0,1,0,0,0,1,1,0,1};
+int song4[] = {0,1,0,0,0,1,1,0,1};
+
 int bpm = 60;
 
-int closed_pos = 110;
-int open_pos = 90;
+int servo0_closed = 110;
+int servo0_opened = 90;
+int servo1_closed = 110;
+int servo1_opened = 90;
+int servo2_closed = 110;
+int servo2_opened = 90;
+int servo3_closed = 110;
+int servo3_opened = 90;
+int servo4_closed = 110;
+int servo4_opened = 90;
+int servo5_closed = 110;
+int servo5_opened = 90;
+int servo6_closed = 110;
+int servo6_opened = 90;
+int servo7_closed = 110;
+int servo7_opened = 90;
+int servo8_closed = 110;
+int servo8_opened = 90;
+
+
 int sleeptime;
 int note_counter = 0;
 int note;
 int number_of_notes;
 
+void play_C(){
+  servo0.write(servo0_closed);
+  servo1.write(servo1_opened);
+  servo2.write(servo2_opened);
+  servo3.write(servo3_opened);
+  servo4.write(servo4_opened);
+  servo5.write(servo5_opened);
+  servo6.write(servo6_opened);
+  servo7.write(servo7_opened);
+  servo8.write(servo8_opened);
+}
+
+void play_D(){
+  servo0.write(servo0_closed);
+  servo1.write(servo1_closed);
+  servo2.write(servo2_opened);
+  servo3.write(servo3_opened);
+  servo4.write(servo4_opened);
+  servo5.write(servo5_opened);
+  servo6.write(servo6_opened);
+  servo7.write(servo7_opened);
+  servo8.write(servo8_opened);  
+}
+
+
 void set_servos(int note){
   switch (note)
   {
       case 0:
-        servo0.write(closed_pos);
+        play_C();
         break;
 
-      default:
-        servo0.write(open_pos);
+      case 1:
+        play_D();
         break;
   }
 }
 
 void setup() {
-  Serial.begin(9600);
-  servo0.attach(9);
-  /*
-  servo1.attach(1);
-  servo2.attach(2);
-  servo3.attach(3);
-  servo4.attach(4);
-  servo5.attach(5);
-  servo6.attach(6);
-  servo7.attach(7);
-  servo8.attach(8);
-*/
-  Serial.begin(9600);
+  Serial.begin(115200);
 
-  int note = melody[note_counter];
-  number_of_notes = sizeof(melody) / sizeof(melody[0]);
+  servo0.attach(PIN_SERVO0);
+  servo1.attach(PIN_SERVO1);
+  servo2.attach(PIN_SERVO2);
+  servo3.attach(PIN_SERVO3);
+  servo4.attach(PIN_SERVO4);
+  servo5.attach(PIN_SERVO5);
+  servo6.attach(PIN_SERVO6);
+  servo7.attach(PIN_SERVO7);
+  servo8.attach(PIN_SERVO8);
+
+  Serial.begin(9600);
+  SerialBT.begin("Virtuoso");
+
+  int note = song1[note_counter];
+  number_of_notes = sizeof(song1) / sizeof(song1[0]);
   sleeptime = (60.0/bpm) * 1000;
   Serial.println(sleeptime);
 }
@@ -59,13 +119,12 @@ void loop() {
   if (note_counter < number_of_notes){
     set_servos(note);
     Serial.println(note);
-    note = melody[++note_counter];
+    note = song1[++note_counter];
     delay(sleeptime);
   }
   
   else {
     Serial.println("END");
-    delay(10000);
   }
 
 }
